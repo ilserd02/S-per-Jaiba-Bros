5a3fbcc3c107bdd5155b40cecff56a869913d948
ilserd02 <ilserd02@gmail.com>
2026-07-23 22:55:39 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,331 +1,127 @@
-// --- ESCENA PRINCIPAL DEL JUEGO ---
-class GameScene extends Phaser.Scene {
-  constructor() {
-    super({ key: 'GameScene' });
-  }
-
-  preload() {
-    // Assets principales (Personaje y bloques)
-    this.load.image('mario', 'assets/mario.png');
-    this.load.image('floorbricks', 'assets/brick.png');
-    this.load.image('stone', 'assets/stone.png');
-    this.load.image('mysteryBox', 'assets/block.png');
-    this.load.image('emptyBox', 'assets/block-empty.png');
-    this.load.image('platform', 'assets/platform.png');
-
-    // Tuberías
-    this.load.image('tube-small', 'assets/pipe-small.png');
-    this.load.image('tube-medium', 'assets/pipe-medium.png');
-    this.load.image('tube-large', 'assets/pipe-large.png');
-    this.load.image('tube-horizontal', 'assets/pipe-horizontal.png');
-
-    // Items y Decoración
-    this.load.image('mushroom', 'assets/mushroom.png');
-    this.load.image('coin', 'assets/coin.png');
-    this.load.image('flower', 'assets/flower.png');
-    this.load.image('star', 'assets/star.png');
-    this.load.image('cloud1', 'assets/cloud.png');
-    this.load.image('hill', 'assets/hill.png');
-    this.load.image('bush', 'assets/bush.png');
-    this.load.image('castle', 'assets/castle.png');
-    this.load.image('flag', 'assets/flag.png');
-
-    // Enemigos
-    this.load.spritesheet('goomba', 'assets/goomba.png', { frameWidth: 16, frameHeight: 16 });
-    this.load.spritesheet('koopa', 'assets/koopa.png', { frameWidth: 16, frameHeight: 24 });
-  }
-
-  create() {
-    const levelWidth = 1700;
-
-    // Asignar el color azul cielo
-    this.cameras.main.setBackgroundColor('#5c94fc');
-
-    // Nubes de fondo
-    for (let x = 100; x < levelWidth; x += 300) {
-      if (this.textures.exists('cloud1')) {
-        this.add.image(x, 40, 'cloud1').setOrigin(0, 0).setScale(0.15);
-      }
-    }
-
-    // Grupos físicos
-    this.floor = this.physics.add.staticGroup();
-    this.mysteryBoxes = this.physics.add.staticGroup();
-    this.mushrooms = this.physics.add.group();
-    this.goombas = this.physics.add.group();
-
-    this.createAnimations();
-
-    // Generar el mapa
-    this.loadMap1_1();
-
-    // Crear personaje principal
-    if (this.textures.exists('mario')) {
-      this.mario = this.physics.add.sprite(50, 100, 'mario')
-        .setOrigin(0.5, 0.5)
-        .setCollideWorldBounds(true)
-        .setGravityY(300);
-        
-      this.mario.setScale(0.163); 
-      this.mario.body.setSize(160, 240);
-      this.mario.body.setOffset(56, 300);
-
-      this.mario.isBig = false; 
-      this.mario.isEating = false; 
-      this.mario.isDead = false;
-
-      // Colisiones del jugador
-      this.physics.add.collider(this.mario, this.floor);
-      this.physics.add.collider(this.mushrooms, this.floor);
-      this.physics.add.collider(this.goombas, this.floor);
-
-      // Golpe a Cajas Misteriosas
-      this.physics.add.collider(this.mario, this.mysteryBoxes, (mario, boxHit) => {
-        if (mario.body.touching.up) {
-          if (boxHit.hasItem) {
-            boxHit.hasItem = false;
-            if (this.textures.exists('emptyBox')) boxHit.setTexture('emptyBox'); 
-            boxHit.refreshBody();
-
-            if (this.cache.audio.exists('sprout')) this.sound.play('sprout');
-
-            const mushroom = this.mushrooms.create(boxHit.x, boxHit.y - 16, 'mushroom');
-            if (mushroom) {
-              mushroom.setOrigin(0.5, 0.5);
-              mushroom.setVelocityX(50);
-            }
-          } else {
-            if (this.cache.audio.exists('bump')) this.sound.play('bump');
-          }
-        }
-      });
-
-      // Recoger Hongo
-      this.physics.add.overlap(this.mario, this.mushrooms, (mario, mushroomHit) => {
-        if (mario.isEating || mario.isDead) return;
-        mushroomHit.destroy(); 
-        this.convertirEnGrande(mario);
-      });
-
-      // Contacto con Enemigos
-      this.physics.add.collider(this.mario, this.goombas, (mario, goombaHit) => {
-        if (mario.isEating || mario.isDead) return;
-        
-        if (mario.body.touching.down && goombaHit.body.touching.up) {
-          mario.setVelocityY(-180); 
-          if (this.cache.audio.exists('kick')) this.sound.play('kick');
-          
-          goombaHit.setVelocityX(0);
-          goombaHit.body.enable = false;
-
-          this.tweens.add({
-            targets: goombaHit,
-            alpha: 0,
-            duration: 300,
-            onComplete: () => { goombaHit.destroy(); }
-          });
-        } else {
-          if (mario.isBig) {
-            mario.isBig = false;
-            mario.setTexture('mario'); 
-            mario.setScale(0.163); 
-            mario.body.setSize(160, 240);
-            mario.body.setOffset(56, 300);
-          } else {
-            mario.isDead = true;
-            if (this.bgMusic) this.bgMusic.stop();
-            mario.body.allowGravity = false;
-            mario.body.setVelocity(0, 0);
-            
-            if (this.cache.audio.exists('gameover')) this.sound.play('gameover');
-          }
-        }
-      });
-
-      // Seguimiento de Cámara
-      this.cameras.main.setBounds(0, 0, levelWidth, config.height);
-      this.cameras.main.startFollow(this.mario);
-    }
-
-    this.physics.world.setBounds(0, 0, levelWidth, config.height);
-    this.keys = this.input.keyboard.createCursorKeys();
-  }
-
-  loadMap1_1() {
-    const mapCreation = [
-      { "macro": "Floor", "x": 0, "width": 552 },
-      { "thing": "Block", "x": 128, "y": 32 },
-      { "thing": "Brick", "x": 160, "y": 32 },
-      { "thing": "Block", "x": 168, "y": 32, "contents": "Mushroom" },
-      { "thing": "Goomba", "x": 176, "y": 8 },
-      { "thing": "Brick", "x": 176, "y": 32 },
-      { "thing": "Block", "x": 176, "y": 64 },
-      { "thing": "Block", "x": 184, "y": 32 },
-      { "thing": "Brick", "x": 192, "y": 32 },
-      { "macro": "Pipe", "x": 224, "height": 16 },
-      { "macro": "Pipe", "x": 304, "height": 24 },
-      { "thing": "Goomba", "x": 340, "y": 8 },
-      { "macro": "Pipe", "x": 368, "height": 32 },
-      { "thing": "Goomba", "x": 412, "y": 8 },
-      { "thing": "Goomba", "x": 422, "y": 8 },
-      { "macro": "Pipe", "x": 456, "height": 32 },
-      { "macro": "Floor", "x": 568, "width": 120 },
-      { "thing": "Brick", "x": 616, "y": 32 },
-      { "thing": "Block", "x": 624, "y": 32, "contents": "Mushroom" },
-      { "thing": "Brick", "x": 632, "y": 32 },
-      { "thing": "Brick", "x": 640, "y": 32 },
-      { "thing": "Goomba", "x": 640, "y": 72 },
-      { "macro": "Floor", "x": 712, "width": 512 },
-      { "thing": "Goomba", "x": 776, "y": 8 },
-      { "thing": "Goomba", "x": 788, "y": 8 },
-      { "thing": "Koopa", "x": 856, "y": 12 },
-      { "thing": "Block", "x": 872, "y": 64, "contents": "Mushroom" },
-      { "thing": "Stone", "x": 1072, "y": 8 },
-      { "thing": "Stone", "x": 1080, "y": 16, "height": 16 },
-      { "thing": "Stone", "x": 1088, "y": 24, "height": 24 },
-      { "thing": "Stone", "x": 1096, "y": 32, "height": 32 },
-      { "macro": "Floor", "x": 1240, "width": 656 },
-      { "macro": "Pipe", "x": 1304, "height": 16 },
-      { "thing": "Goomba", "x": 1392, "y": 8 },
-      { "macro": "Pipe", "x": 1432, "height": 16 },
-      { "thing": "Castle", "x": 1584, "y": 0 }
-    ];
-
-    mapCreation.forEach(item => {
-      const posX = item.x || 0;
-      const posY = config.height - (item.y || 0) - 16;
-
-      if (item.macro === "Floor") {
-        for (let x = posX; x < posX + item.width; x += 16) {
-          if (this.textures.exists('floorbricks')) {
-            this.floor.create(x, config.height - 8, 'floorbricks').setOrigin(0, 0.5).refreshBody();
-          }
-        }
-      } 
-      else if (item.macro === "Pipe") {
-        let pipeTexture = 'tube-small';
-        if (item.height >= 32) pipeTexture = 'tube-large';
-        else if (item.height >= 24) pipeTexture = 'tube-medium';
-
-        if (this.textures.exists(pipeTexture)) {
-          this.floor.create(posX, config.height - (item.height / 2), pipeTexture).refreshBody();
-        }
-      } 
-      else if (item.thing === "Block") {
-        if (this.textures.exists('mysteryBox')) {
-          const box = this.mysteryBoxes.create(posX, posY, 'mysteryBox').setOrigin(0, 0.5).refreshBody();
-          box.hasItem = (item.contents === "Mushroom");
-        }
-      } 
-      else if (item.thing === "Brick") {
-        if (this.textures.exists('floorbricks')) {
-          this.floor.create(posX, posY, 'floorbricks').setOrigin(0, 0.5).refreshBody();
-        }
-      } 
-      else if (item.thing === "Stone") {
-        if (this.textures.exists('stone')) {
-          const h = item.height || 8;
-          for (let yOffset = 0; yOffset < h; yOffset += 8) {
-            this.floor.create(posX, config.height - 16 - yOffset, 'stone').setOrigin(0, 0.5).refreshBody();
-          }
-        }
-      } 
-      else if (item.thing === "Goomba") {
-        if (this.textures.exists('goomba')) {
-          const goomba = this.goombas.create(posX, config.height - 24 - (item.y || 0), 'goomba').setOrigin(0.5, 0.5);
-          goomba.setVelocityX(-40);
-          goomba.setCollideWorldBounds(true);
-        }
-      } 
-      else if (item.thing === "Koopa") {
-        if (this.textures.exists('koopa')) {
-          const koopa = this.goombas.create(posX, config.height - 24 - (item.y || 0), 'koopa').setOrigin(0.5, 0.5);
-          koopa.setVelocityX(-35);
-          koopa.setCollideWorldBounds(true);
-        }
-      } 
-      else if (item.thing === "Castle") {
-        if (this.textures.exists('castle')) {
-          this.add.image(posX, config.height - 48, 'castle').setOrigin(0, 1);
-        }
-      }
-    });
-  }
-
-  createAnimations() {
-    if (!this.anims.exists('goomba-walk') && this.textures.exists('goomba')) {
-      this.anims.create({
-        key: 'goomba-walk',
-        frames: this.anims.generateFrameNumbers('goomba', { start: 0, end: 1 }),
-        frameRate: 5,
-        repeat: -1
-      });
-    }
-  }
-
-  convertirEnGrande(mario) {
-    mario.isBig = true;
-    mario.isEating = false;
-    mario.body.allowGravity = true; 
-    
-    if (this.cache.audio.exists('powerup')) this.sound.play('powerup');
-
-    mario.setScale(0.187); 
-    mario.y -= 30; 
-    mario.body.setSize(160, 180);
-    mario.body.setOffset(56, 360);
-    mario.body.reset(mario.x, mario.y);
-  }
-
-  update() {
-    this.goombas.children.iterate(goomba => {
-      if (goomba && goomba.body && goomba.body.enable && this.anims.exists('goomba-walk')) {
-        goomba.anims.play('goomba-walk', true);
-      }
-    });
-
-    if (!this.mario || this.mario.isDead || this.mario.isEating) return;
-
-    if (this.keys.left.isDown) {
-      this.mario.setVelocityX(-120); 
-      this.mario.flipX = true;
-    } else if (this.keys.right.isDown) {
-      this.mario.setVelocityX(120);  
-      this.mario.flipX = false;
-    } else {
-      this.mario.setVelocityX(0);     
-    }
-
-    if (this.keys.up.isDown && this.mario.body.touching.down) {
-      this.mario.setVelocityY(-300);
-      if (this.cache.audio.exists('jump')) this.sound.play('jump');
-    }
-
-    if (this.mario.y >= config.height) {
-      this.mario.isDead = true;
-      if (this.bgMusic) this.bgMusic.stop();
-      if (this.cache.audio.exists('gameover')) this.sound.play('gameover');
-    }
-  }
-}
-
-// --- CONFIGURACIÓN PRINCIPAL DE PHASER ---
+// --- CONFIGURACIÓN PRINCIPAL ---
 const config = {
   type: Phaser.AUTO,
-  parent: 'game', // Conecta con <div id="game"></div> de tu index.html
+  parent: 'game',
   width: 256,
   height: 240,
-  backgroundColor: '#5c94fc',
+  backgroundColor: '#5c94fc', // El azul que ya ves
   pixelArt: true,
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 500 },
+      gravity: { y: 300 },
       debug: false
     }
   },
   scene: [GameScene]
 };
 
-// Inicializar el juego
 const game = new Phaser.Game(config);
+
+// --- ESCENA PRINCIPAL ---
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'GameScene' });
+  }
+
+  preload() {
+    // Carga de imágenes
+    this.load.image('mario', 'assets/mario.png');
+    this.load.image('floorbricks', 'assets/brick.png');
+    this.load.image('mysteryBox', 'assets/block.png');
+    this.load.image('emptyBox', 'assets/block-empty.png');
+    this.load.image('tube-small', 'assets/pipe-small.png');
+    this.load.image('mushroom', 'assets/mushroom.png');
+    this.load.image('castle', 'assets/castle.png');
+    this.load.spritesheet('goomba', 'assets/goomba.png', { frameWidth: 16, frameHeight: 16 });
+  }
+
+  create() {
+    const levelWidth = 1700;
+
+    // Asegurar color del cielo
+    this.cameras.main.setBackgroundColor('#5c94fc');
+
+    // Grupos con físicas
+    this.floor = this.physics.add.staticGroup();
+    this.mysteryBoxes = this.physics.add.staticGroup();
+    this.mushrooms = this.physics.add.group();
+    this.goombas = this.physics.add.group();
+
+    // 1. Crear el suelo a lo largo del nivel
+    for (let x = 0; x < levelWidth; x += 16) {
+      if (this.textures.exists('floorbricks')) {
+        // Coloca los bloques justo en el borde inferior (y = 232)
+        this.floor.create(x, 232, 'floorbricks').setOrigin(0, 0.5).refreshBody();
+      }
+    }
+
+    // 2. Crear al personaje principal
+    if (this.textures.exists('mario')) {
+      // Aparece en x: 40, y: 180 (justo arriba del suelo)
+      this.mario = this.physics.add.sprite(40, 180, 'mario')
+        .setOrigin(0.5, 0.5)
+        .setCollideWorldBounds(true);
+
+      // Ajuste de escala adecuado para la pantalla de 256x240
+      this.mario.setScale(0.08); 
+      this.mario.body.setSize(180, 200);
+
+      // Colisión con el piso
+      this.physics.add.collider(this.mario, this.floor);
+      this.physics.add.collider(this.goombas, this.floor);
+
+      // Seguimiento de Cámara
+      this.cameras.main.setBounds(0, 0, levelWidth, 240);
+      this.cameras.main.startFollow(this.mario);
+    }
+
+    // Cargar bloques del mapa
+    this.loadMap1_1();
+
+    this.physics.world.setBounds(0, 0, levelWidth, 240);
+    this.keys = this.input.keyboard.createCursorKeys();
+  }
+
+  loadMap1_1() {
+    // Bloques de prueba a una altura visible (y: 160)
+    const objetos = [
+      { type: 'Block', x: 100, y: 160 },
+      { type: 'Brick', x: 116, y: 160 },
+      { type: 'Block', x: 132, y: 160 },
+      { type: 'Pipe', x: 180, y: 208 }
+    ];
+
+    objetos.forEach(obj => {
+      if (obj.type === 'Block' && this.textures.exists('mysteryBox')) {
+        this.mysteryBoxes.create(obj.x, obj.y, 'mysteryBox').refreshBody();
+      } else if (obj.type === 'Brick' && this.textures.exists('floorbricks')) {
+        this.floor.create(obj.x, obj.y, 'floorbricks').refreshBody();
+      } else if (obj.type === 'Pipe' && this.textures.exists('tube-small')) {
+        this.floor.create(obj.x, obj.y, 'tube-small').refreshBody();
+      }
+    });
+
+    if (this.mario && this.mysteryBoxes) {
+      this.physics.add.collider(this.mario, this.mysteryBoxes);
+    }
+  }
+
+  update() {
+    if (!this.mario) return;
+
+    if (this.keys.left.isDown) {
+      this.mario.setVelocityX(-120);
+      this.mario.flipX = true;
+    } else if (this.keys.right.isDown) {
+      this.mario.setVelocityX(120);
+      this.mario.flipX = false;
+    } else {
+      this.mario.setVelocityX(0);
+    }
+
+    if (this.keys.up.isDown && this.mario.body.touching.down) {
+      this.mario.setVelocityY(-250);
+    }
+  }
+}
